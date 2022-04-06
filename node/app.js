f9f9f3e19b036d609c2cf954e6a54b0309ce3d8a
Aprisyta Gupta <aprisyta10@gmail.com>
2022-04-06 00:28:04 -0400

    test

--- node/app.js
+++ node/app.js
@@ -1,45 +1,76 @@
+console.log(performance.now());
 console.log("Node is starting");
+// setTimeout(function() {}, 10000);
 var UDP_Socket = require('dgram');
 
-var server = UDP_Socket.createSocket('udp4');
+var socketServer = UDP_Socket.createSocket('udp4');
 
-var s = UDP_Socket.createSocket('udp4');
-// const NODE_1_PORT = process.env.NODE_1_PORT;
 const PORT = 8000;
-server.bind(PORT);
-console.log("Listening on the port", + PORT);
+const SERVER_DETAIL = process.env.SERVER_NAME
+const SERVER_ARRAY=['Node1', 'Node2', 'Node3']
 
-const function_to_demonstrate_multithreading = () => {
-    for (let i = 0; i < 5; i++) {
-        console.log("Hi Executing Dummy function : " + i)
-    }
-    setTimeout(300)
-}
+socketServer.bind(PORT, () => {
+  socketServer.setRecvBufferSize(9999999);
+});
 
-const listener = (server) => {
-   
-    server.on('message',function(msg, rinfo) {
-    console.log('I received this msg from the client: ' + msg)
+// const function_to_demonstrate_multithreading = () => {
+//     for (let i = 0; i < 5; i++) {
+//         console.log("Hi Executing Dummy function : " + i)
+//     }
+//     setTimeout(300)
+// }
+
+const listener = (socketServer) => {
+    socketServer.on('message',function(msg, rinfo) {
+      console.log(`I received this msg: ${msg}`)
     });
 }
-    
-if(process.env.SERVER_NUMBER === '1'){
-        console.log("This is node 1")
-        listener(server)
-
-   
-}
-
-if(process.env.SERVER_NUMBER === '2'){
-    console.log("This is node 2")
-    for (let i = 0; i < 5; i++) {
-        s.send(Buffer.from('hello this is a msg from the client with counter' + i), PORT, 'Node1', function(err, bytes){
+listener(socketServer)
+const sender = (socketServer, destination, increment) => {
+  const data = `Sending message from ${SERVER_DETAIL}: ITER ${increment}`
+  var msg = new Buffer.from(data)
+  socketServer
+    .send(
+      msg,
+      0,
+      msg.length,
+      PORT,
+      destination,
+      function(err, bytes){
         if (err) throw err;
-        console.log('UDP message' + i + 'sent to ' + 'Node1' +':'+ PORT);
-        });    
-      }
-        console.log("closing client");
-    
+        console.log(`UDP message sent to ${destination}`);
+    });
+  setTimeout(function() {}, 2000);
+}
+
+for (var i = 0; i < SERVER_ARRAY.length; i++) {
+  if (SERVER_ARRAY[i] !== SERVER_DETAIL) {
+    for (var j = 0; j < 10; j++) {
+      sender(socketServer, SERVER_ARRAY[i], j)
+    }
+  }
 }
 
 
+// const serversToBeCalled = SERVER_ARRAY.filter(server => SERVER_DETAIL !== server)
+// const promises = serversToBeCalled.map(s => sender(socketServer, s))
+// const x = Promise.allSettled(promises)
+
+
+
+// Promise.allSettled()
+// if(process.env.SERVER_NUMBER === '1'){
+//     console.log("This is node 1")
+//     listener(server)
+// }
+//
+// if(process.env.SERVER_NUMBER === '2'){
+//     console.log("This is node 2")
+//     for (let i = 0; i < 5; i++) {
+//         s.send(Buffer.from('hello this is a msg from the client with counter' + i), PORT, 'Node1', function(err, bytes){
+//         if (err) throw err;
+//         console.log('UDP message' + i + 'sent to ' + 'Node1' +':'+ PORT);
+//         });
+//       }
+//         console.log("closing client");
+// }
