b5b8a1d85fa7816ddd9449443c9b61146fa319bc
Aprisyta Gupta <aprisyta10@gmail.com>
2022-04-07 20:22:02 -0400

    Update app.js

--- node/app.js
+++ node/app.js
@@ -17,13 +17,32 @@ const node = {
   timeout: process.env.TIMEOUT
 }
 
+let voteTally = 0
+
+const incrementTerm = (jump) => {
+  node.term = node.term + jump
+}
+
+const changeState = (state) => {
+  console.log(`${state} Line 27`);
+  node.state = state
+}
+
+const changeVotedFor = (votedFor) => {
+  node.votedFor = votedFor
+}
+
+const getLastLogIndex = () => {
+  return node.logs.length - 1
+}
+
 const createVoteRequest = () => {
   const msg = { ...msgJson }
   msg.sender_name = node.name
   msg.term = node.term
   msg.request = 'VOTE_REQUEST'
   msg.candidateId = node.name
-  msg.lastLogIndex = -1
+  msg.lastLogIndex = getLastLogIndex()
   msg.lastLogTerm = 0
   return JSON.stringify(msg)
 }
@@ -38,33 +57,36 @@ const createVoteAcnowledgement = (responseInd, voteRequestMsg) => {
   return JSON.stringify(msg)
 }
 
-let voteTally = 0
-
-const incrementTerm = (jump) => {
-  node.term = node.term + jump
-}
-
-const changeState = (state) => {
-  node.state = state
+const createHeartbeats = () => {
+  const msg = { ...msgJson }
+  msg.sender_name = node.name
+  msg.term = node.term
+  msg.request = 'APPEND_RPC'
+  msg.prevLogIndex = getLastLogIndex()
+  msg.prevLogTerm = 0
+  return JSON.stringify(msg)
 }
 
 const vote = (msg) => {
   if (!msg && node.state === STATES.CANDIDATE) {
-    node.votedFor = node.name
+    changeVotedFor(node.name)
     voteTally = 1
     return
   }
-  else if (msg.term > node.term) {
-    if (node.votedFor === '') {
-      incrementTerm(msg.term - node.term)
-      node.votedFor = msg.candidateId
-      return createVoteAcnowledgement(true, msg)
-    }
-    else
-      return createVoteAcnowledgement(false, msg)
+  if (msg.term > node.term) {
+    incrementTerm(msg.term - node.term)
+    changeVotedFor('')
+    changeState(STATES.FOLLOWER)
   }
-  // logic to check log entries
-  return createVoteAcnowledgement(false, msg)
+  let logOKInd = false
+  if (msg.lastLogIndex >= getLastLogIndex()) {
+    logOKInd = true
+    // do something when logs[] implemented
+  }
+  if (msg.term === node.term && node.votedFor === '' && logOKInd)
+    return createVoteAcnowledgement(true, msg)
+  else
+    return createVoteAcnowledgement(false, msg)
 }
 
 socketServer.bind(PORT, () => {
@@ -86,7 +108,7 @@ const sender = (socketServer, destination, data) => {
       });
 }
 
-function sendMessage(data) {
+function broadcast(data) {
   const serversToBeCalled = SERVER_ARRAY.filter(server => node.name !== server)
   const promises = serversToBeCalled.map(server => sender(socketServer, server, data))
   Promise.allSettled(promises)
@@ -96,23 +118,38 @@ const voteRequest = () => {
   incrementTerm(1)
   changeState(STATES.CANDIDATE)
   vote()
+  // check logs
   const data = createVoteRequest()
-  sendMessage(data)
+  broadcast(data)
 }
 
 let timeoutTimer = null;
+let heartbeatTimer = null;
 
 function setElectionTimeout() {
-  timeoutTimer = setTimeout(function reset() {
+  timeoutTimer = setTimeout(function resetElectionTimer() {
     voteRequest()
-    timeoutTimer = setTimeout(reset, node.timeout)
+    timeoutTimer = setTimeout(resetElectionTimer, node.timeout)
   }, node.timeout)
+}
+
+const sendHeartbeats = () => {
+  const heartbeat = createHeartbeats()
+  broadcast(heartbeat)
+}
+
+function setHeartbeatsTimeout() {
+  heartbeatTimer = setTimeout(function resetHeartbeatTimer() {
+    sendHeartbeats()
+    heartbeatTimer = setTimeout(resetHeartbeatTimer, 150)
+  }, 150)
 }
 
 const becomeLeader = () => {
   console.log("I am in becomeLeader");
   changeState(STATES.LEADER)
   clearTimeout(timeoutTimer)
+  setHeartbeatsTimeout()
   voteTally = 0
   console.log(`Line numver : 116 ${JSON.stringify(node)}`);
 }
@@ -121,24 +158,35 @@ const listener = async (socketServer) => {
   // await new Promise(resolve => setTimeout(resolve, 3000));
   setElectionTimeout()
   socketServer.on('message',function(msg, rinfo) {
-    clearTimeout(timeoutTimer)
     msg = JSON.parse(msg.toString())
     console.log(`Line 125: ${JSON.stringify(msg)}`);
     if (msg.request === 'VOTE_REQUEST') {
       const responseVoteMsg = vote(msg)
+      if (JSON.parse(responseVoteMsg).granted)
+        setElectionTimeout()
       sender(socketServer, msg.sender_name, responseVoteMsg)
     }
-    else if (msg.request === 'VOTE_ACK' && msg.granted) {
-      if (node.state === STATES.LEADER) return;
-      voteTally++
-      if (voteTally >= SERVER_ARRAY.length/2) {
-        becomeLeader()
+    else if (msg.request === 'VOTE_ACK') {
+      console.log(`Line 167: ${node.state}`);
+      if (msg.term > node.term) {
+        clearTimeout(timeoutTimer)
+        incrementTerm(msg.term - node.term)
+        changeState(STATES.FOLLOWER)
+        changeVotedFor('')
+        setElectionTimeout()
+      }
+      else if (node.state === STATES.CANDIDATE && msg.granted) {
+        voteTally++
+        console.log(`${voteTally} votes received by ${node.name} for ${node.term}`);
+        if (voteTally >= SERVER_ARRAY.length/2) {
+          becomeLeader()
+        }
       }
     }
     else if (msg.request === 'APPEND_RPC') {
-      // do something here
+      clearTimeout(timeoutTimer)
+      setElectionTimeout()
     }
-    setElectionTimeout()
   });
 }
 
